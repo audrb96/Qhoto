3e60e5804b59ea1ba1d2ae8dc48b0cacec38b661
sanghyeonsh <ksh3967@naver.com>
2022-11-08 14:46:57 +0900

    [FEAT] add video column api (https://ssafy.atlassian.net/browse/S07P31A707-171)

--- qhoto_api/src/main/java/com/qhoto/qhoto_api/api/service/FeedService.java
+++ qhoto_api/src/main/java/com/qhoto/qhoto_api/api/service/FeedService.java
@@ -5,6 +5,7 @@ import com.qhoto.qhoto_api.domain.*;
 import com.qhoto.qhoto_api.domain.type.CommentStatus;
 import com.qhoto.qhoto_api.domain.type.FeedLikePK;
 import com.qhoto.qhoto_api.domain.type.FeedStatus;
+import com.qhoto.qhoto_api.domain.type.FeedType;
 import com.qhoto.qhoto_api.dto.request.CreateCommentReq;
 import com.qhoto.qhoto_api.dto.request.CreateFeedReq;
 import com.qhoto.qhoto_api.dto.request.FeedAllReq;
@@ -78,6 +79,7 @@ public class FeedService {
                 // '내'가 눌렀는지 확인해야 하므로 userInfo에서 가져옴
                 .likeStatus((feedLikeRepository.findById(userInfo.getId()).isPresent())?LikeStatus.LIKE:LikeStatus.UNLIKE)
                 .commentList(commentResList)
+                .feedType(feed.getFeedType())
                 .build();
 
         return feedDetailRes;
@@ -106,18 +108,18 @@ public class FeedService {
         log.info("quest = {}", quest.getDuration());
         User user = userRepository.findUserById(userInfo.getId()).orElseThrow(()-> new NoUserByIdException("no user by id"));
         String dirName = "feed/image/"+user.getEmail();
-        S3upload(createFeedReq, quest, user, dirName);
+        S3upload(createFeedReq, quest, user, dirName, FeedType.IMAGE);
     }
 
     public void postVideoFeed(CreateFeedReq createFeedReq,User userInfo) throws IOException {
         Quest quest = questRepository.findQuestById(createFeedReq.getQuestId()).orElseThrow(()-> new NoQuestByIdException("no quest by id"));;
         User user = userRepository.findUserById(userInfo.getId()).orElseThrow(()-> new NoUserByIdException("no user by id"));
         String dirName = "feed/video/input/"+user.getEmail();
-        S3upload(createFeedReq, quest, user, dirName);
+        S3upload(createFeedReq, quest, user, dirName, FeedType.VIDEO);
 
     }
 
-    private void S3upload(CreateFeedReq createFeedReq, Quest quest, User user, String dirName) throws IOException {
+    private void S3upload(CreateFeedReq createFeedReq, Quest quest, User user, String dirName, FeedType feedType) throws IOException {
         s3Utils.upload(createFeedReq.getFeedImage(),dirName);
         dirName = S3Utils.CLOUD_FRONT_DOMAIN_NAME+"/"+dirName;
         Feed feed = Feed.builder()
@@ -136,6 +138,7 @@ public class FeedService {
                 .score(quest.getScore())
                 .difficulty(quest.getDifficulty())
                 .duration(quest.getDuration())
+                .feedType(feedType)
                 .build();
         Exp exp = expRepository.findAllByTypeCodeAndUserId(quest.getQuestType().getCode(),user.getId());
         exp.addPoint(quest.getScore());
