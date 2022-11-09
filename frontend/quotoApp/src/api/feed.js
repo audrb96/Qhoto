5aa1351cc3f9812cc919f4ddace95443dd4d12ff
HYUNGJIN <accent680@naver.com>
2022-11-09 13:21:52 +0900

    [FEAT]feed_design

--- frontend/quotoApp/src/api/feed.js
+++ frontend/quotoApp/src/api/feed.js
@@ -11,6 +11,15 @@ async function getAllFeeds(params, success, fail) {
     .then(success)
     .catch(fail);
 }
+async function getFriendsFeeds(params, success, fail) {
+  await api
+    .get('/api/feed/friend', {
+      params: {condition: params[1], duration: params[0]},
+      headers: await createHeaders(),
+    })
+    .then(success)
+    .catch(fail);
+}
 
 async function getSelectedFeed(feedId, success, fail) {
   await api
@@ -60,6 +69,7 @@ async function setFeedMission(success, fail) {
 export {
   getAllFeeds,
   getSelectedFeed,
+  getFriendsFeeds,
   setFeedLike,
   setFeedDisLike,
   setComment,
