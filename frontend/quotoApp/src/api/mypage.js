0fad9457b01c97b2c3bcc3b4005a8de2f79287ff
박영준 <jjoon0306@naver.com>
2022-11-11 13:39:28 +0900

    merge conflict

--- frontend/quotoApp/src/api/mypage.js
+++ frontend/quotoApp/src/api/mypage.js
@@ -1,4 +1,4 @@
-import {apiInstance, fileApiInstance, createHeaders} from '.';
+import {apiInstance, fileApiInstance} from '.';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = apiInstance();
@@ -54,17 +54,64 @@ api.interceptors.response.use(
   },
 );
 
+// request interceptor
+fileApi.interceptors.request.use(async config => {
+  if (!config.headers) return config;
+
+  let token = null;
+
+  if (config.url === REFRESH_URL) {
+    token = await AsyncStorage.getItem('refreshToken');
+  } else {
+    token = await AsyncStorage.getItem('accessToken');
+  }
+
+  if (token !== null) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// response interceptor
+fileApi.interceptors.response.use(
+  res => res,
+  async err => {
+    console.log(err.response);
+    const {
+      config,
+      response: {
+        data: {code},
+      },
+    } = err;
+
+    /** 1 */
+    if (config.url === REFRESH_URL || code !== 'U002' || config.sent) {
+      return Promise.reject(err);
+    }
+
+    /** 2 */
+    config.sent = true;
+
+    const response = await api.post(REFRESH_URL);
+    const accessToken = response.data.accessToken;
+    const refreshToken = response.data.refreshToken;
+
+    await AsyncStorage.setItem('accessToken', accessToken, () => {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    });
+    await AsyncStorage.setItem('refreshToken', refreshToken);
+
+    return fileApi(config);
+  },
+);
+
 async function getUserInfoApi(success, fail) {
   await api.get('/api/me').then(success).catch(fail);
 }
 
 async function editMyProfileApi(newUserInfo, success, fail) {
-  console.log('newUserInfo', newUserInfo);
-  await fileApi
-    // Body
-    .put('/api/user', newUserInfo)
-    .then(success)
-    .catch(fail);
+  await fileApi.put('/api/user', newUserInfo).then(success).catch(fail);
 }
 
 async function duplicateTestApi(nickname, success, fail) {
