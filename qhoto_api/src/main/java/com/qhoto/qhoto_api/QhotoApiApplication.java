399bdc5726c22ae66e0967aba57a8eb9f03ffaf7
박명규 <audrb96@naver.com>
2022-11-12 14:04:25 +0900

    [fix] scheduler timezone

--- qhoto_api/src/main/java/com/qhoto/qhoto_api/QhotoApiApplication.java
+++ qhoto_api/src/main/java/com/qhoto/qhoto_api/QhotoApiApplication.java
@@ -2,11 +2,13 @@ package com.qhoto.qhoto_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 //@PropertySource("application-local.yaml")
+@EnableAsync
 public class QhotoApiApplication {
 
 	public static void main(String[] args) {
