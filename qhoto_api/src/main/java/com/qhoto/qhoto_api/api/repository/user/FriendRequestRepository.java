3f3ae5c99fe5a2023477dd5194cb04fa02dbc2b4
박명규 <audrb96@naver.com>
2022-11-18 15:09:41 +0900

    [FIX] friend request error

--- qhoto_api/src/main/java/com/qhoto/qhoto_api/api/repository/user/FriendRequestRepository.java
+++ qhoto_api/src/main/java/com/qhoto/qhoto_api/api/repository/user/FriendRequestRepository.java
@@ -4,6 +4,7 @@ import com.qhoto.qhoto_api.domain.FriendRequest;
 import com.qhoto.qhoto_api.domain.User;
 import com.qhoto.qhoto_api.domain.type.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -24,7 +25,7 @@ public interface FriendRequestRepository extends JpaRepository<FriendRequest, Lo
      * @param status
      * @return {@link Optional<FriendRequest>}
      */
-    Optional<FriendRequest> findByRequestUserAndResponseUserAndStatusNot(User reqUser, User ResUser, RequestStatus status);
+    Optional<FriendRequest> findByRequestUserAndResponseUser(User reqUser, User ResUser);
 
     /**
      * 친구 요청유저, 받은 유저, 요청 상태에 맞는 FriendRequest를 가져온다.
@@ -37,4 +38,8 @@ public interface FriendRequestRepository extends JpaRepository<FriendRequest, Lo
 
     @Query("select f from FriendRequest f where (f.requestUser.id=:userId and f.responseUser.id=:friendId) or (f.requestUser.id=:friendId and f.responseUser.id=:userId)")
     List<FriendRequest> findByUserIdAndFriendId(@Param("userId") Long id, @Param("friendId") Long friendId);
+
+    @Modifying
+    @Query("update FriendRequest f set f.status=:status where f.requestUser=:reqUser and f.responseUser =:resUser")
+    int updateStatus(@Param("status") RequestStatus status, @Param("reqUser") User reqUser, @Param("resUser") User resUser);
 }
